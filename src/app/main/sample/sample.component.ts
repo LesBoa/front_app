1199793f32167d118ddd620bf93590f9aa22f31e
Jannou <jannou59@gmail.com>
2018-12-06 19:56:59 +0100

    :sparkles: change lang turkish to french

--- src/app/main/sample/sample.component.ts
+++ src/app/main/sample/sample.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 import { FuseTranslationLoaderService } from '@fuse/services/translation-loader.service';
 
 import { locale as english } from './i18n/en';
-import { locale as turkish } from './i18n/tr';
+import { locale as french } from './i18n/fr';
 
 @Component({
     selector   : 'sample',
@@ -21,6 +21,6 @@ export class SampleComponent
         private _fuseTranslationLoaderService: FuseTranslationLoaderService
     )
     {
-        this._fuseTranslationLoaderService.loadTranslations(english, turkish);
+        this._fuseTranslationLoaderService.loadTranslations(english, french);
     }
 }
