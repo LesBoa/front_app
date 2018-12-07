ba662d2829105044bd0a552d9afe76380d479b2e
Jannou <jannou59@gmail.com>
2018-12-07 06:17:08 +0100

    :bug: fix services

--- src/app/main/logbook/logbook.service.ts
+++ src/app/main/logbook/logbook.service.ts
@@ -3,19 +3,20 @@ import {Logbook} from "../../model/model.logbook";
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {environment } from 'environments/environment';
 import {Observable, of} from 'rxjs';
+import {ConfigGetterService} from "../services/config-getter.service";
 
 @Injectable()
 export class LogbookService {
 
-  constructor(public http: HttpClient) { }
+  constructor(public http: HttpClient,
+              private configGetter: ConfigGetterService) { }
 
   getAll(){
-    return this.http.get(environment.apiUrl+'/journals');
+    return this.http.get(`${this.configGetter.urlServer}/journals`);
   }
 
   create(title: String, description: String){
-    return this.http.post(environment.apiUrl+'/journals',{title, description}).subscribe( ret => {
-
+    return this.http.post(`${this.configGetter.urlServer}/journals`,{title, description}).subscribe( ret => {
     });
   }
-}
+}``
--- src/app/main/stock/stock.service.ts
+++ src/app/main/stock/stock.service.ts
@@ -3,24 +3,26 @@ import {Logbook} from "../../model/model.logbook";
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {environment } from 'environments/environment';
 import {Observable, of} from 'rxjs';
+import {ConfigGetterService} from "../services/config-getter.service";
 
 @Injectable()
 export class StockService {
 
-  constructor(public http: HttpClient) { }
+  constructor(public http: HttpClient,
+              private configGetter: ConfigGetterService,
+              ) { }
 
   getAll(){
-    return this.http.get(environment.apiUrl+'/stocks');
+    return this.http.get(`${this.configGetter.urlServer}/stocks`);
   }
 
   createStock(name: String, quantite: number, unite: String){
-    return this.http.post(environment.apiUrl+'/stocks',{name, quantite, unite}).subscribe( ret => {
-
+    return this.http.post(`${this.configGetter.urlServer}'/stocks`,{name, quantite, unite}).subscribe( ret => {
     });
   }
 
   createAction(name: String, variation: number, icon: String){
-    return this.http.post(environment.apiUrl+'/actions',{name, variation, icon}).subscribe( ret => {
+    return this.http.post(`${this.configGetter.urlServer}/actions`,{name, variation, icon}).subscribe( ret => {
 
     });
   }
